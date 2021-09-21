8fde6e7333c61d4616804f31d603e14d71560db2
TechoMonkey <90660877+TechoMonkey@users.noreply.github.com>
2021-09-21 12:23:33 -0500

    Add files via upload

---
+++ Homework 2.cpp
@@ -0,0 +1,18 @@
+// Homework 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student {
+    string fName;
+    string lName;
+    float grade;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
