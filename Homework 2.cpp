2f76b4a3328ffb83e52a8649f6ae0595fbd8d473
TechoMonkey <90660877+TechoMonkey@users.noreply.github.com>
2021-09-22 02:31:30 -0500

    Update Homework 2.cpp

--- Homework 2.cpp
+++ Homework 2.cpp
@@ -3,16 +3,58 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include<vector>
+
+
 using namespace std;
 
 class student {
+
     string fName;
     string lName;
     float grade;
+public:
+    string getfName()
+    {
+        return fName;
+   }
+    string getlName()
+    {
+        return lName;
+    }
+    float getgrade()
+    {
+        return grade;
+    }
+   
 };
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<student>myStudents;
+    for (int i = 0; i < myStudents.size(); i++) 
+    {
+
+      
+
+        myStudents.push_back(student());
+    }
+    ifstream myfile("example.txt");
+
+    string fName;
+    
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, fName))
+            cout << fName << endl;
+
+    }
+
+    else
+        cout << "unable to open file\n";
+    
+    return(0);
 }
